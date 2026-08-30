84cc568f61e3984e3f60c17c9f7dd2e3bd4a0137
Ribbit0asis <sota19shi99@gmail.com>
2026-08-30 20:21:07 +0900

    chg flag

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-  const SHOW_MAP = true;
+  const SHOW_MAP = false;
 
   const ASSET_VERSION = "1";
-  const PLANNED_BOOTHS_KEY = "eventMapViewer.plannedBooths";
+  const PLANNED_BOOTHS_KEY = "eventInfoViewer.plannedBooths";
 
   let allBooths = [];
   let overlayInitialized = false;
